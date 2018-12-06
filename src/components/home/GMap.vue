fe4b7cb42be69d524c05463363e9befa7d595a9d
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-12-06 16:13:04 +0800

    route guarding

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from 'firebase'
+import db from '@/firebase/init'
 export default {
     name: 'GMap',
     data(){
@@ -26,8 +27,38 @@ export default {
         }
     },
     mounted(){
-        this.renderMap()
-        console.log(firebase.auth().currentUser)
+        let user = firebase.auth().currentUser
+        console.log('user')
+        console.log(user)
+
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(pos => {
+                this.lat = pos.coords.latitude
+                this.lng = pos.coords.longitude
+                db.collection('users').where('user_id','==',user.uid).get()
+                .then(snapshot =>{
+                    snapshot.forEach((doc)=>{
+                        console.log(doc.id)
+                        db.collection('users').doc(doc.id).update({
+                            geolocation:{
+                                lat:pos.coords.latitude,
+                                lng:pos.coords.longitude
+                            }
+                        })
+                    })
+                }).then(()=>{
+
+                this.renderMap()
+                })
+            },(err)=>{
+                console.log(err)
+                this.renderMap()
+            },{maxmiumAge:60000, timeout:3000})
+        }else{
+            this.renderMap()
+        }
+        
+      
       
     }
 }
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,10 +2,10 @@
     <div class="navbar">
         <nav class="deep-purple darken-1">
             <div class="container">
-                <a href="" class="brand-logo left">GeoNinjas!</a>
-                <ul class="right">
+                <router-link :to="{name:'GMap'}">GeoNinjas</router-link>
+              <ul class="right">
                     <li><router-link :to="{name:'Signup'}">Signup</router-link></li>
-                    <li><a href="">Login</a></li>
+                    <li><router-link :to="{name:'Login'}">Login</router-link></li>
                     <li><a @click="logout">Logout</a></li>
                 </ul>
             </div>
@@ -24,7 +24,7 @@ export default {
     methods:{
         logout(){
             firebase.auth().signOut().then(()=>{
-                 this.$router.push({name:'Signup'})
+                 this.$router.push({name:'Login'})
             })
         }
     },
@@ -34,5 +34,16 @@ export default {
 }
 </script>
 <style>
+.login{
+    max-width: 400px;
+    margin-top: 60px;
 
+}
+
+.login h2{
+    font-size: 2.4em;
+}
+.login .field{
+    margin-bottom: 16px;
+}
 </style>
