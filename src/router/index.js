4dfbae614a1c8644b9b046e31a88379575760479
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-12-07 11:02:03 +0800

    start user comments

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,19 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
-
+import ViewProfile from '@/components/profile/ViewProfile'
+import firebase from 'firebase'
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta:{
+        requiresAuth:true
+      }
     },
     {
       path: '/signup',
@@ -22,6 +26,30 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path:'/profile/:id',
+      name:'ViewProfile',
+      component:ViewProfile,
+      meta:{
+        requiresAuth:true
+      }
     }
   ]
 })
+
+router.beforeEach((to,from,next)=>{
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    //check auth state of user
+    let user = firebase.auth().currentUser
+    if (user) {
+      next()
+    }else{
+      next({name:'Login'})
+    }
+  }else{
+    next()
+  }
+})
+
+export default router
